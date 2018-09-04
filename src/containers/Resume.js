35b44fb25b5604df32dc6fc6112dd73da3417c12
zfv200 <zach.vary@flatironschool.com>
2018-09-04 17:36:20 -0400

    added resume

--- src/containers/Resume.js
+++ src/containers/Resume.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import { Document, Page } from 'react-pdf'
 
-class Resume extends React.Component{
+class Resume extends React.Component {
+  state = {
+    pageNumber: 1,
+  }
 
-  render(){
+  render() {
+    const { pageNumber } = this.state;
+
     return (
-      <div>
-        <h1>Resume!</h1>
+      <div className="resume">
+        <Document file="ZVaryResume.pdf">
+          <Page pageNumber={pageNumber} />
+        </Document>
       </div>
-    )
+    );
   }
 }
 
