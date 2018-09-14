5a584009c6cc27e56620b53fe8b21f60d4c0884f
zfv200 <zach.vary@flatironschool.com>
2018-09-14 09:21:54 -0400

    added links

--- src/components/Project.js
+++ src/components/Project.js
@@ -24,17 +24,30 @@ class Project extends React.Component{
     }
   }
 
+  githubBackEnd = () => {
+    if (this.props.ghlB){
+      return <a href={this.props.ghlB}>Github API</a>
+    }
+  }
+
+  demo = () => {
+    if (this.props.demo){
+      return <a href={this.props.demo}>Demo</a>
+    }
+  }
+
   render(){
     return(
       <div>
         <h1 className="projectTitle" onClick={this.handleClick}>{this.props.title}</h1>
         <p className="stack">{this.props.stack}</p>
+        <a href={this.props.bhlF}>Github</a> {this.githubBackEnd()} {this.demo()}
         {this.state.toggled ?
           <FadeIn>
           <div>
           <p className="centered">{this.props.description}</p>
             <div className="list">
-              <ul>
+              <ul className="left">
               <li>{this.props.line1}</li>
               <li>{this.props.line2}</li>
               <li>{this.props.line3}</li>
