20e9861ca4ecdca8a429d9cc2f158c827e429bcd
zfv200 <zach.vary@flatironschool.com>
2018-09-19 09:22:43 -0400

    summary update

--- src/containers/About.js
+++ src/containers/About.js
@@ -5,7 +5,7 @@ class About extends React.Component{
 
   summary = () => {
     return (
-      <p>Dedicated Full Stack Web Developer with a commitment to collaborative learning, education, development and production.
+      <p>Dedicated Full Stack Web Developer with a commitment to collaborative learning, education, and development.
 Integrating experience in Javascript, React, Ruby/Rails with a background in philosophy, I am happiest when working with a
 team of driven and creative developers committed to reaching new heights with software and web
 technology.</p>
